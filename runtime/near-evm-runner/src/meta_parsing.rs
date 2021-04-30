e19980e1afa6faaec7c40bb92585be00a0977480
Michael Birch <michael@near.org>
2021-04-30 19:06:17 +0000

    cargo fmt

--- runtime/near-evm-runner/src/meta_parsing.rs
+++ runtime/near-evm-runner/src/meta_parsing.rs
@@ -387,12 +387,10 @@ fn arg_to_abi_token(ty: &ArgType, arg: &RlpValue, methods: &MethodAndTypes) -> R
         ArgType::Uint | ArgType::Int | ArgType::Bool => {
             value_to_abi_token(arg, |b| Ok(ABIToken::Uint(ethabi::Uint::from_big_endian(&b))))
         }
-        ArgType::Address => {
-            value_to_abi_token(arg, |b| {
-                let address = ethabi::Address::from_slice(b);
-                Ok(ABIToken::Address(address))
-            })
-        }
+        ArgType::Address => value_to_abi_token(arg, |b| {
+            let address = ethabi::Address::from_slice(b);
+            Ok(ABIToken::Address(address))
+        }),
         ArgType::Array { inner, length: None } => list_to_abi_token(arg, |l| {
             let mut tokens = vec![];
             for arg in l {
